1c38317e52977c2a0bde80355266b68bc0aa3c54
kulsoq00 <kulsoq00@gmail.com>
2025-07-25 15:59:20 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'a1e72fd93ed59f56e6332813b9f8dcae';
+const API_KEY = 'd1224e64f26af381a2741a6bf8e91334';
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
